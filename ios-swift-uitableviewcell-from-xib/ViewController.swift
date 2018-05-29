a7ad3d250c29fc516fd795b49767b1b15aebbe17
demo <vncode247@gmail.com>
2018-05-29 19:15:39 +0700

    Initial Commit

---
+++ ios-swift-uitableviewcell-from-xib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-swift-uitableviewcell-from-xib
+//
+//  Created by demo on 5/29/18.
+//  Copyright © 2018 VNCode247. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
